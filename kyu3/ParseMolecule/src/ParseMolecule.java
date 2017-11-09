8f03a2c92e0fe30db279502c03403eda083698bf
Hp i7 <Hp i7@DESKTOP-33OMQ5B>
2017-11-09 12:00:05 +0200

    Just the empty file before I add the code

---
+++ kyu3/ParseMolecule/src/ParseMolecule.java
@@ -0,0 +1,39 @@
+import java.util.Map;
+import java.util.HashMap;
+
+class ParseMolecule {
+    
+    public static Map<String,Integer> getAtoms(String formula) {
+        //Get rid of brackets
+        int bracketLocation=Math.max(Math.max(formula.indexOf('('), formula.indexOf('{')),formula.indexOf('['));
+        while (bracketLocation!=-1) {            
+            int start=bracketLocation;
+            int end=formula.lastIndexOf(opposite(formula.charAt(start)));
+            int repeat = Integer.parseInt(formula.charAt(end+1)+"");
+            String part=formula.substring(start+1, end);
+            formula=formula.substring(0,start)+formula.substring(end+2);
+            for (int i = 0; i < repeat; i++) {
+                formula=formula.substring(0, start)+part+formula.substring(start);
+            }
+            bracketLocation=Math.max(Math.max(formula.indexOf('('), formula.indexOf('{')),formula.indexOf('['));
+        }
+        
+        //Get rid of numbers
+        while (formula.matches(".*\\d+.*")) {            
+            System.out.println("found");
+        }
+        
+        return new HashMap<String,Integer>();
+    }
+
+    private static char opposite(char charAt) {
+        switch(charAt){
+            case '{':
+                return '}';
+            case '[':
+                return ']';
+            default:
+                return ')';
+        }
+    }
+}
