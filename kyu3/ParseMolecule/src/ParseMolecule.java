ada785fdd9934981a45f8971034a09bb35647f71
COOLERMASTERWIN\Bennie <19217226>
2017-11-10 19:35:23 +0200

    Updates

--- kyu3/ParseMolecule/src/ParseMolecule.java
+++ kyu3/ParseMolecule/src/ParseMolecule.java
@@ -6,6 +6,7 @@ class ParseMolecule {
 
     public static Map<String, Integer> getAtoms(String formula) throws IllegalArgumentException {
         //Get rid of brackets
+        System.out.println("");
         int bracketLocation = Math.max(Math.max(formula.lastIndexOf('('), formula.lastIndexOf('{')), formula.lastIndexOf('['));
         while (bracketLocation != -1) {
             int start = bracketLocation;
@@ -25,6 +26,14 @@ class ParseMolecule {
             }
             bracketLocation = Math.max(Math.max(formula.lastIndexOf('('), formula.lastIndexOf('{')), formula.lastIndexOf('['));
         }
+        
+        
+        
+        
+        asdfknm;k;
+        
+        a;lsdkfj;
+        
         if (formula.contains(")") || formula.contains("]") || formula.contains("}"))
             throw new IllegalArgumentException();
 
