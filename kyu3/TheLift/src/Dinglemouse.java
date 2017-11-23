ed2a0d7093c0f0d709611318c681fd358930037f
COOLERMASTERWIN\Bennie <19217226>
2017-11-23 20:48:58 +0200

    Updates

--- kyu3/TheLift/src/Dinglemouse.java
+++ kyu3/TheLift/src/Dinglemouse.java
@@ -1,8 +1,27 @@
+
+import java.util.ArrayList;
+
 public class Dinglemouse {
 
-  public static int[] theLift(final int[][] queues, final int capacity) {
-    // Your code here
-    return new int[0];
-  }
+    public static int[] theLift(final int[][] queues, final int capacity) {
+        ArrayList<Integer> stop = new ArrayList<>();
+        boolean direction = true; //True up, False down
+        int floor = 0;
+
+        while (!isEmpty(queues)) {
+            break;
+        }
+
+        return new int[0];
+    }
+
+    private static boolean isEmpty(int[][] queues) {
+        for (int i = 0; i < queues.length; i++) {
+            if (queues[i].length!=0) {
+                return false;
+            }            
+        }
+        return true;
+    }
 
 }
