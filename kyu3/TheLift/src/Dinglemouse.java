4e171e20413c56188eb2c2bd5a26b248fddd2286
COOLERMASTERWIN\Bennie <19217226>
2017-11-23 20:34:17 +0200

    Start Project - Base

--- kyu3/TheLift/src/Dinglemouse.java
+++ kyu3/TheLift/src/Dinglemouse.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Bennie
- */
 public class Dinglemouse {
-    
+
+  public static int[] theLift(final int[][] queues, final int capacity) {
+    // Your code here
+    return new int[0];
+  }
+
 }
