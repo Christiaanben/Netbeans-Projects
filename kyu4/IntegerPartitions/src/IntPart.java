1769581e9467c2c9d364fab66952fce24c21bf80
COOLERMASTERWIN\Bennie <19217226>
2017-11-29 01:03:43 +0200

    Finish

--- kyu4/IntegerPartitions/src/IntPart.java
+++ kyu4/IntegerPartitions/src/IntPart.java
@@ -1,55 +1,42 @@
-
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
-import java.util.SortedSet;
 import java.util.TreeSet;
 
-
 public class IntPart {
+    public static TreeSet<Integer>[] memo;
 
     public static String part(int n) {
-        SortedSet<Integer> set=partProd(n);
-        ArrayList<Integer> list=new ArrayList<>();
-        list.addAll(set);
+        memo=new TreeSet[n+1];
+        ArrayList<Integer> list=new ArrayList<>(partProd(n));
         
-        int range=set.last()-1;
+        int range=list.get(list.size()-1)-1;
         
         double avg=0;
-        for (Integer l : list) {
+        for (Integer l : list) 
             avg+=l;
-        }
         avg/=list.size();
         
-        double med;
-        if (list.size()%2==0) {
-            med=(list.get(list.size()/2)+list.get(list.size()/2-1))/2;
-        }else{
-            med=list.get(list.size()/2);
-        }
-        System.out.println(set);
-        String str=String.format("Range: %d Average: %.2f Median: %.2f", range,avg,med);
-        System.out.println(str);
-        return str;
+        double med=list.get(list.size()/2);
+        if (list.size()%2==0) 
+            med=(med+list.get(list.size()/2-1))/2.0;
+        
+        return String.format("Range: %d Average: %.2f Median: %.2f", range,avg,med);
     }
     
     public static TreeSet<Integer> partProd(int n){
         if (n<2) 
             return new TreeSet<>(Arrays.asList(1));
-        if (n==2)
-            return new TreeSet<>(Arrays.asList(2,1));
-        if (n==3)
-            return new TreeSet<>(Arrays.asList(3,2,1));
-        if (n==4)
-            return new TreeSet<>(Arrays.asList(4,3,2,1));
         TreeSet<Integer> set = new TreeSet<>();
         for (int i = 0; i < n; i++) {
-            TreeSet<Integer> auxSet=partProd(i);
-            for (Integer aux: auxSet) {
+            TreeSet<Integer> auxSet;
+            if (memo[i]==null)
+                auxSet=partProd(i);
+            else
+                auxSet=memo[i];
+            for (Integer aux: auxSet)
                 set.add((n-i)*aux);
-            }
         }
+        memo[n]=set;
         return set;
     }
 }
--- kyu4/IntegerPartitions/test/IntPartTest.java
+++ kyu4/IntegerPartitions/test/IntPartTest.java
@@ -4,19 +4,35 @@ import org.junit.Test;
 public class IntPartTest {
 	
   @Test
-  public void Numbers_Small1() {
+  public void Numbers1() {
 	  assertEquals("Range: 1 Average: 1.50 Median: 1.50", IntPart.part(2));
   }	
   @Test
-  public void Numbers_Small2() {
+  public void Numbers2() {
 	  assertEquals("Range: 2 Average: 2.00 Median: 2.00", IntPart.part(3));
   }	
   @Test
-  public void Numbers_Small3() {
+  public void Numbers3() {
 	  assertEquals("Range: 3 Average: 2.50 Median: 2.50", IntPart.part(4));
   }	
   @Test
-  public void Numbers_Small4() {
+  public void Numbers4() {
 	  assertEquals("Range: 5 Average: 3.50 Median: 3.50", IntPart.part(5));
+  }	
+  @Test
+  public void Numbers5() {
+	  assertEquals("Range: 8 Average: 4.75 Median: 4.50", IntPart.part(6));
+  }
+  @Test
+  public void Numbers11() {
+	  assertEquals("Range: 53 Average: 19.69 Median: 16.00", IntPart.part(11));
+  }
+  @Test
+  public void Numbers21() {
+	  assertEquals("Range: 2186 Average: 358.10 Median: 197.00", IntPart.part(21));
+  }
+  @Test
+  public void Numbers26() {
+	  assertEquals("Range: 13121 Average: 1500.90 Median: 625.00", IntPart.part(26));
   }
 }
