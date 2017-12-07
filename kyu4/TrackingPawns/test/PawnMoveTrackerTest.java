a0d0c204c1ab4340cde476aaf572d882d6f60151
Hp i7 <Hp i7@DESKTOP-33OMQ5B>
2017-12-07 17:50:24 +0200

    Start Tracking Pawn

---
+++ kyu4/TrackingPawns/test/PawnMoveTrackerTest.java
@@ -0,0 +1,32 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class PawnMoveTrackerTest {
+
+    @Test
+    public void exampleTest1() {
+        String[][] expected = { {".",".",".",".",".",".",".","."},
+                                {".","p","p",".","p","p","p","p"},
+                                {"p",".",".","p",".",".",".","."},
+                                {".",".",".",".",".",".",".","."},
+                                {".",".",".",".","P",".",".","."},
+                                {".",".",".",".",".",".",".","."},
+                                {"P","P","P","P",".","P","P","P"},
+                                {".",".",".",".",".",".",".","."} };
+        assertEquals(expected, PawnMoveTracker.movePawns(new String[] {"e3", "d6", "e4", "a6"}));
+    }
+    
+    @Test
+    public void exampleTest2() {
+        String[][] expected = { {".",".",".",".",".",".",".","."},
+                                {"p","p","p",".","p","p","p","p"},
+                                {".",".",".",".",".",".",".","."},
+                                {".",".",".",".",".",".",".","."},
+                                {".",".",".",".","p",".",".","."},
+                                {".",".",".","P",".",".",".","."},
+                                {"P","P","P",".",".","P","P","P"},
+                                {".",".",".",".",".",".",".","."} };
+        assertEquals(expected, PawnMoveTracker.movePawns(new String[] {"e4", "d5", "d3", "dxe4"}));
+    }
+}
